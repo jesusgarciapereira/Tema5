37755b822d1a5299c197f0f6b14d9f9d481b4b29
Jesús <jesus.garciap@iesnervion.es>
2024-02-27 18:08:06 +0100

    Ejercicio03 hecho y probado

---
+++ src/parte1/MainPunto.java
@@ -0,0 +1,42 @@
+package parte1;
+
+/**
+ * Clase Main para probar la clase del Ejercicio03 del Tema 5 parte1
+ */
+public class MainPunto {
+
+	/**
+	 * Metodo main
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// Distancia entre los dos puntos
+		double distancia;
+
+		// Creo un objeto de clase Punto
+		Punto p1 = new Punto(2, 3);
+		// Llamo a la función imprime() para mostrarlo
+		p1.imprime();
+
+		// Llamo a la función setXY para modificar ambas coordenadas
+		p1.setXY(3, 2);
+		// Lo muestro modificado
+		p1.imprime();
+
+		// Llamo a la función desplaza() para mover el punto
+		p1.desplaza(4, 3);
+		// Lo muestro desplazado
+		p1.imprime();
+		
+		// Creo un nuevo punto
+		Punto p2 = new Punto(4, 1);
+		// Llamo a la función distancia() con los dos puntos y se lo asigno a la variable distancia
+		distancia= p1.distancia(p2);
+		// Muestro la distancia
+		System.out.println(distancia);
+
+	}
+
+}
---
+++ src/parte1/Punto.java
@@ -0,0 +1,80 @@
+package parte1;
+
+/**
+ * Clase correspondiente al Ejercicio 3: Punto
+ */
+public class Punto {
+
+	/**
+	 * Coordenada X
+	 */
+	private int cordX;
+
+	/**
+	 * Coordenada Y
+	 */
+	private int cordY;
+
+	/**
+	 * Constructor con todos los atributos como parametro
+	 * 
+	 * @param cordX
+	 * @param cordY
+	 */
+	public Punto(int cordX, int cordY) {
+		this.cordX = cordX;
+		this.cordY = cordY;
+	}
+
+	/**
+	 * Funcion imprime: Imprime por pantalla las coordenadas
+	 */
+	public void imprime() {
+		// Muestra las coordenadas tal que así
+		System.out.println("(" + cordX + ", " + cordY + ")");
+
+	}
+
+	/**
+	 * Funcinon setXY: Literalmente, un setter de ambos atributos
+	 * 
+	 * @param x Nuevo valor de la coordenada X
+	 * @param y Nuevo valor de la coordenada Y
+	 */
+	public void setXY(int x, int y) {
+		this.cordX = x;
+		this.cordY = y;
+
+	}
+
+	/**
+	 * Funcinon desplaza: Desplaza la coordenada la cantidad indicada en el
+	 * parametro
+	 * 
+	 * @param dx Cuanto se incrementa la coordenada X
+	 * @param dy Cuanto se incrementa la coordenada Y
+	 */
+	public void desplaza(int dx, int dy) {
+		this.cordX += dx;
+		this.cordY += dy;
+
+	}
+
+	/**
+	 * Funcion distancia: Calcula y devuelve la distancia entre el propio objeto y
+	 * otro objeto indicado en el parametro
+	 * 
+	 * @param p Objeto de clase Punto
+	 * @return La distancia entre el objeto al que se aplica la funcion y el objeto del parametro
+	 */
+	public double distancia(Punto p) {
+		// Numero que indica la distancia entre los dos puntos
+		double distancia;
+
+		// Asignamos a la variable la siguiente fórmula
+		distancia = Math.sqrt(Math.pow(p.cordX - this.cordX, 2) + Math.pow(p.cordY - this.cordY, 2));
+
+		// Devolverá la distancia entre los dos puntos
+		return distancia;
+	}
+}
