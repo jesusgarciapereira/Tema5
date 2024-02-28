1166b86b4b4da8c183317d33630b3abcd468716e
Jesús <jesus.garciap@iesnervion.es>
2024-02-28 12:18:25 +0100

    Ejercicio04 Hecho y probado

--- src/parte1/ejercicio04/Articulo.java
+++ src/parte1/ejercicio04/Articulo.java
@@ -8,7 +8,7 @@ public class Articulo {
 	/**
 	 * Nombre del articulo
 	 */
-	private String nombre;
+	private String nombre = "";
 	/**
 	 * Precio del articulo (sin IVA)
 	 */
@@ -50,4 +50,86 @@ public class Articulo {
 			System.out.println("Cantidad de artículos no válida");
 	}
 
+	/**
+	 * Funcion muestraInformacion: Muestra la informacion detallada del articulo
+	 */
+	public void muestraInformacion() {
+		// Mensaje con el nombre del artículo
+		System.out.println("Nombre del artículo: " + nombre);
+		// Mensaje con el precio del artículo (sin IVA)
+		System.out.println("Precio del artículo (sin IVA): " + precioSinIVA + "€");
+		// Mensaje con la cantidad de artículos que quedan en el almacén
+		System.out.println("Cuántos quedan en el almacén: " + cuantosQuedan);
+		// Salto de línea
+		System.out.println();
+	}
+
+	/**
+	 * Funcion getPVP: Devuelve el precio del producto con el IVA incluido
+	 * 
+	 * @return Precio de venta al publico (PVP)
+	 */
+	public double getPVP() {
+		// Precio de venta al público que devolverá la función
+		double precioPVP;
+		// Asignamos a la variable la siguiente fórmula
+		precioPVP = precioSinIVA * (IVA + 100) / 100;
+		// Devolverá dicho valor
+		return precioPVP;
+
+	}
+
+	/**
+	 * Funcion getPVPDescuento: Devuelve el PVP con el descuento del parametro
+	 * 
+	 * @param descuento Porcentaje de descuento que se aplicara
+	 * @return Precio de venta al publico con el descuento
+	 */
+	public double getPVPDescuento(double descuento) {
+		// Precio con descuento que devolverá la función
+		double precioPVPConDescuento;
+		// PVP sin el descuento
+		double precioPVPSinDescuento;
+
+		// Asignamos al precio sin descuento la siguiente fórmula
+		precioPVPSinDescuento = (precioSinIVA * (IVA + 100) / 100);
+		// Asignamos al precio con descuento la siguiente fórmula
+		precioPVPConDescuento = precioPVPSinDescuento * (100 - descuento) / 100;
+
+		// Devolverá el valor del precio con descuento
+		return precioPVPConDescuento;
+
+	}
+
+	/**
+	 * Funcion vender: Indica si se puede llevar a cabo la venta o no y, si
+	 * es posible, resta al atributo cuantosQuedan la cantidad a vender
+	 * 
+	 * @param x Cantidad de articulos que se venderan
+	 * @return True o false segun si es posible llevar a cabo la venta
+	 */
+	public boolean vender(int x) {
+		
+		// Variable que devolverá la función
+		boolean sePuedeVender = false;
+
+		// Si el valor del parámetro es mayor que 0 y menor o igual que la cantidad que queda
+		if (x > 0 && x <= cuantosQuedan) {
+			// Se asignará nuestra variable como true
+			sePuedeVender = true;
+			// Y se le resta a cuantos quedan la cantidad que se venderá
+			cuantosQuedan -= x;
+		}
+		// Devuelve el valor de nuestro boolean
+		return sePuedeVender;
+	}
+	
+	/**
+	 * Funcion almacenar: Actualiza el atributo cuantosQuedan incrementandole la cantidad introducida en el parametro
+	 * @param x Cantidad que incrementaremos al atributo
+	 */
+	public void almacenar (int x) {
+		cuantosQuedan += x;
+		
+	}
 }
--- src/parte1/ejercicio04/Main.java
+++ src/parte1/ejercicio04/Main.java
@@ -11,9 +11,75 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Articulo a1 = new Articulo ("", 0, -7);
-		
 
+		// Precio de venta al público
+		double precioPVP;
+		// Descuento que aplicaremos
+		int descuento = 50;
+		// Precio de venta al público con descuento
+		double precioPVPConDescuento;
+		// Boolean que indicará si es posible vender
+		boolean sePuedeVender;
+		// Cantidad que almacenaremos
+		int cantAlmacenar = 2;
+
+		// Creamos un objeto con datos no válidos para comprobar que se muestren los
+		// mensajes
+		Articulo a0 = new Articulo(null, 0, -7);
+		// Salto de línea
+		System.out.println();
+
+		// Creamos un objeto con datos válidos
+		Articulo a1 = new Articulo("Pantalones vaqueros", 20, 57);
+
+		// Mostramos la información del artículo recién creado
+		a1.muestraInformacion();
+
+		// Llamamos a la función getPVP() y asignamos el valor devuelto a la variable
+		// del precio de venta al público
+		precioPVP = a1.getPVP();
+		// Y la mostramos
+		System.out.println("Precio con IVA: " + precioPVP + "€");
+		// Salto de línea
+		System.out.println();
+
+		// Llamamos a la función getPVPDescuento() con el descuento aplicado y asignamos
+		// el valor devuelto a la
+		// variable del precio con descuento
+		precioPVPConDescuento = a1.getPVPDescuento(descuento);
+		// Y la mostramos
+		System.out.println("Precio con el " + descuento + "% de descuento: " + precioPVPConDescuento + "€");
+		// Salto de línea
+		System.out.println();
+
+		// Llamamos a la función vender() con la cantidad a vender y le asignamos
+		// el valor devuelto a nuestro boolean
+		sePuedeVender = a1.vender(10);
+		// Aplicamos un operador ternario para mostrar un mensaje u otro según el valor
+		// de nuestro boolean
+		System.out.println(sePuedeVender ? "Venta realizada con éxito" : "No es posible llevar a cabo la venta");
+		// Salto de línea
+		System.out.println();
+
+		// Mostramos la información del artículo con la venta hecha
+		a1.muestraInformacion();
+
+		// Si la cantidad a almacenar es menor que 0
+		if (cantAlmacenar < 0)
+			// Mostraremos este mensaje
+			System.out.println("No es posible almacenar esa cantidad");
+		// En caso contrario
+		else {
+			// Mostraremos este mensaje
+			System.out.println("Almacenamiento realizado");
+			// Y llamamos a la función almacenar() para actualizar los datos
+			a1.almacenar(cantAlmacenar);
+		}
+		// Salto de línea
+		System.out.println();
+
+		// Mostramos la información del artículo con el producto nuevo almacenado
+		a1.muestraInformacion();
 	}
 
 }
